76f6e98865246506a321c81e91b9f89d0d37b002
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-15 16:43:23 -0700

    basic layout

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,24 @@
-angular.module('huddle', []);
+angular.module('huddle', [
+  'ngRoute',
+])
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/templates/home.html',
+      controller: 'HomeController'
+    })
+    .when('/meeting', {
+      templateUrl: 'app/templates/meeting.html',
+      controller: 'MeetingController'
+    })
+    .when('/create', {
+      templateUrl: 'app/templates/new-meeting.html',
+      controller: 'NewMeetingController'
+    })
+    .when('/groups', {
+      templateUrl: 'app/templates/groups.html',
+      controller: 'GroupsController'
+    })
+    // default for unknown routes
+    .otherwise({redirectTo: '/'});
+});
