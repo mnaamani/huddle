5b109b6f73210dc45c790b75fd593ea850b1ae19
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-16 15:37:45 -0700

    added webrtc

--- public/app/app.js
+++ public/app/app.js
@@ -186,6 +186,22 @@ angular.module('huddle', [
     clearInterval(updateInterval);
   });
 
+  var webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remotesVideos',
+    // immediately ask for camera access
+    autoRequestMedia: true
+  });
+
+
+  // we have to wait until it's ready
+  webrtc.on('readyToCall', function () {
+    // you can name it anything
+    webrtc.joinRoom("huddle"+$scope.meetingId);
+  });
+
 })
 .controller('HomeController', function($scope, Meetings){
   //show all Meetings
