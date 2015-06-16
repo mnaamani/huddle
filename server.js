0185ff27e5bb33353362abe231adcbc7e1d0f8bc
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-16 09:40:45 -0700

    added mongodb store for sessions

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var Slackey = require('slackey');
+var MongoStore = require('connect-mongo')(session);
 
 var SlackStrategy = require('passport-slack').Strategy;
 //TODO: get these from environment vars
@@ -18,7 +19,10 @@ var slackAPI = new Slackey({
 var app = express();
 
 app.use(session({
-  secret: SESSION_SECRET
+  secret: SESSION_SECRET,
+  store: new MongoStore({
+    url: 'mongodb://localhost/huddle-session'
+  })
 }));
 
 passport.serializeUser(function(user, done) {
@@ -102,7 +106,7 @@ app.get('/api/whoami', ensureAuthenticatedAPI, function(req, res){
   response.send(200, req.user);
 });
 
-//proxying api to slack
+//proxying api calls to slack
 app.get('/api/team/list', ensureAuthenticatedAPI, function(req, res){
 
   var slack = slackAPI.getClient(req.user.slackAccessToken);
@@ -116,4 +120,32 @@ app.get('/api/team/list', ensureAuthenticatedAPI, function(req, res){
   });
 });
 
+//meeting api
+app.post('/api/meetings/new', ensureAuthenticatedAPI, function(req, res){
+  console.log('creating new meeting for:', req.body)
+  //send out invites
+
+  res.send(201,{
+    id: '77777'
+  });
+});
+
+app.post('/api/meetings/control/:id', ensureAuthenticatedAPI, function(req, res){
+  //verify owner of meeting is issueing control commands
+  res.send(201,{
+    id: '77777'
+  });
+});
+
+app.get('/api/meetings/info/:id', ensureAuthenticatedAPI, function(req, res){
+  //retreive meeting info from database
+  //verify that user is authorised
+  //set user presence 'in meeting'
+  //send back latest meeting status info
+  res.send(200,{
+    data: 123
+  });
+});
+
+//start the server
 app.listen(8000);
